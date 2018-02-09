9bdc83dd9ac09ba54b8fc130bc1842853c4fe6c0
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-02-09 09:16:28 -0700

    added some of the panel.

--- src/history/view/HistoryPanel.java
+++ src/history/view/HistoryPanel.java
@@ -29,16 +29,21 @@ public class HistoryPanel extends JPanel
 		appLayout = new SpringLayout();
 		
 		viewLocalHistory = new JButton("History");
+		appLayout.putConstraint(SpringLayout.EAST, viewLocalHistory, -111, SpringLayout.EAST, this);
 		viewLocalSites = new JButton("Sites");
-		appLayout.putConstraint(SpringLayout.NORTH, viewLocalHistory, 0, SpringLayout.NORTH, viewLocalSites);
-		appLayout.putConstraint(SpringLayout.WEST, viewLocalHistory, 40, SpringLayout.EAST, viewLocalSites);
-		appLayout.putConstraint(SpringLayout.WEST, viewLocalSites, 115, SpringLayout.WEST, this);
-		appLayout.putConstraint(SpringLayout.SOUTH, viewLocalSites, -92, SpringLayout.SOUTH, this);
-		descriptionArea = new JTextArea("This is an app which takes your location and shows you local history and sites");
+		appLayout.putConstraint(SpringLayout.NORTH, viewLocalSites, 0, SpringLayout.NORTH, viewLocalHistory);
+		appLayout.putConstraint(SpringLayout.EAST, viewLocalSites, -67, SpringLayout.WEST, viewLocalHistory);
+		//descriptionArea = new JTextArea("This is an app which takes your location and shows you local history and sites");
+		appLayout.putConstraint(SpringLayout.SOUTH, descriptionArea, 6, SpringLayout.NORTH, viewLocalHistory);
 		titleLabel = new JLabel("History locator");
-		appLayout.putConstraint(SpringLayout.NORTH, titleLabel, 10, SpringLayout.NORTH, this);
-		appLayout.putConstraint(SpringLayout.WEST, titleLabel, 174, SpringLayout.WEST, this);
-		locationLabel = new JLabel();
+		appLayout.putConstraint(SpringLayout.NORTH, viewLocalHistory, 6, SpringLayout.SOUTH, titleLabel);
+		appLayout.putConstraint(SpringLayout.NORTH, descriptionArea, 9, SpringLayout.SOUTH, titleLabel);
+		appLayout.putConstraint(SpringLayout.NORTH, titleLabel, 32, SpringLayout.NORTH, this);
+		appLayout.putConstraint(SpringLayout.WEST, titleLabel, 172, SpringLayout.WEST, this);
+		locationLabel = new JLabel("location");
+		appLayout.putConstraint(SpringLayout.NORTH, locationLabel, -26, SpringLayout.SOUTH, this);
+		appLayout.putConstraint(SpringLayout.WEST, locationLabel, 190, SpringLayout.WEST, this);
+		appLayout.putConstraint(SpringLayout.SOUTH, locationLabel, -10, SpringLayout.SOUTH, this);
 		
 		this.History = new JPanel();
 		
@@ -52,7 +57,7 @@ public class HistoryPanel extends JPanel
 	{
 		setLayout(appLayout);
 		
-		this.setBackground(Color.BLACK);
+		this.setBackground(Color.BLUE);
 		this.add(descriptionArea);
 		this.add(viewLocalHistory);
 		this.add(viewLocalSites);
