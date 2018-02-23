54e12aa82a2a10a84b01ea8422070d05f554fd6f
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-02-23 08:43:15 -0700

    Setting up controller

--- src/history/controller/HistoryController.java
+++ src/history/controller/HistoryController.java
@@ -1,12 +1,25 @@
 package history.controller;
 
+
+import history.view.HistoryFrame;
+import history.model.ProvideWebsite;
+import history.model.ProvideLocation;
+
 public class HistoryController 
 {
 	//private data members
 	
+	private ProvideLocation location;
+	private HistoryFrame appFrame;
+	private ProvideWebsite Website;
+	
+	
+	
 	public HistoryController()
 	{
-		
+		location = new ProvideLocation();
+		appFrame = new HistoryFrame(this);
+		Website = new ProvideWebsite();
 		
 	}
 	
