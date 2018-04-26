835bc65555de39a2abab6cfe14569b43b93eaeeb
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-04-26 08:10:46 -0600

    less errors

--- src/history/view/HistoryPanel.java
+++ src/history/view/HistoryPanel.java
@@ -1,7 +1,6 @@
 package history.view;
 
 import history.controller.HistoryController;
-import java.awt.Color;
 
 import javax.swing.*;
 
@@ -65,7 +64,152 @@ public class HistoryPanel extends JPanel implements HyperlinkListener
 			forwardButton.setEnabled(true);
 			buttonPanel.add(forwardButton);
 			URLField = new JTextField(35);
-			
+			fileMenu.add(fileExitMenuItem);
+			menu.add(fileMenu);
+			setJMenuBar(menu);
+			forwardButton.setEnabled(true);
+			buttonPanel.add(forwardButton);
+			URLField = new JTextField(35);
+			buttonPanel.add(URLField);
+			JButton returnButton = new JButton("Return");
+			buttonPanel.add(returnButton);
+			private void actionEscape()
+			  {
+			  System.exit(0);
+			  }
+			private void actionBack()
+			  {
+			  	URL currentUrl = displayEditorPane.getPage();
+				int pageIndex = pageList.indexOf(currentUrl.toString());
+				try
+				{
+					showPage(new URL((String) pageList.get(pageIndex -1)), false);
+				}
+				catch(Exception e)
+				{
+					
+				}
+			  }
+			private void actionForward()
+			  {
+			  	URL currentUrl = displayEditorPane.getPage();
+				int pageIndex = pageList.indexOf(currentUrl.toString());
+				try
+				{
+					showPage(new URL((String) pageList.get(pageIndex +1)), false);
+				}
+				catch(Exception e)
+				{
+					
+				}
+			  }
+			 private void actionGo()
+			    {
+			    	URL verifiedUrl = verifyUrl(URLField.getText());
+				if(verifiedUrl != null)
+				{
+					showPage(verifiedUrl, true);
+				}
+				else
+				{
+					showError("Not a valid URL, Try Again!");
+				}
+			    }
+			 private void showError(String errorMessage)
+			   {
+			   	JOptionPane.showMessageDialog(this, errorMessage, "Error", JOptionPane.ERROR_MESSAGE);
+			   }
+			 private URL verifyUrl(String url)
+			 {
+			 	if(!url.toLowerCase().startsWith("http://"))
+			 	{
+			 		return null;
+			 	}
+			 	URL verifiedUrl = null;
+			 	try
+			 	{
+			 		verifiedUrl = new URL(url);
+			 	}
+			 	catch(Exception e)
+			 	{
+			 	return null;
+			 	}
+			 return verifiedUrl;
+			 	
+			 }
+			 private void showPage(URL pageUrl, boolean addToList)
+			 {
+			 	setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+			 		try
+			 		{
+			 			URL currentURL = displayEditorPane.getPage();
+			 			displayEditorPane.setPage(pageUrl);
+			 			URL newUrl = displayEditorPane.getPage();
+			 			//abstraction
+			 			if(addToList)
+			 			{
+			 				int listSize = pageList.size();
+			 				if(listSize>0)
+			 				{
+			 					int pageIndex = pageList.indexOf(currentUrl.toString());
+			 					if(pageIndex < listSize - 1)
+			 					{
+			 						for(int index = listSize -1; index > pageIndex; index --)
+			 						{
+			 						pageList.remove(index);
+			 						}
+			 					}
+			 				}
+			 				pageList.add(newUrl.toString());
+			 			}
+			 		}
+			 			
+			 			catch(Exception e)
+			 			{
+			 			showError("Unable to load da page");
+			 			}
+			 			finally
+			 			{
+			 			setCursor(Cursor.getDefaultCursor());
+			 			}
+			 		
+			 		//endAbstraction
+			 		URLField.setText(newUrl.toString());
+			 		updatesButtons();
+			 	
+			 	
+			 }
+			 private void updateButtons()
+			 {
+			 	if(pageList.size() < 2)
+				{
+				backButton.setEnabled(true);
+				forwardButton.setEnabled(true);
+				}
+				else
+				{
+				URL currentUrl = displayEditorPane.getPage();
+				int pageIndex = pageList.indexOf(currentUrl.toString());
+				backButton.setEnabled(pageIndex > 0);
+				forwardButton.setEnabled(pageIndex < (pageList.size() -1));
+				}
+			 } 
+	}
+	public void hyperlinkUpdate(HyperlinkEvent event)
+	{
+	HyperlinkEvent.EventType eventType = event.getEventType();
+	if (eventType == HyperlinkEvent.EventType.ACTIVATED)
+	{
+		if(event instanceof HTMLFrameHyperlinkEvent)
+		{
+			HTMLFrameHyperlinkEvent linkEvent = (HTMLFrameHyperlinkEvent) event;
+			HTMLDocument document = (HTMLDocument) displayEditorPane.getDocument();
+			document.processHTMLFrameHyperlinkEvent(linkEvent);
+		}
+		else
+		{
+			showPage(event.getURL(), true);
+		}
 	}
 	
 	private void setupPanel()
@@ -81,18 +225,19 @@ public class HistoryPanel extends JPanel implements HyperlinkListener
 		this.add(descriptionArea);
 		this.add(close);
 		this.add(inputField);
-		 fileMenu.add(fileExitMenuItem);
-			menu.add(fileMenu);
-			setJMenuBar(menu);
-			forwardButton.setEnabled(true);
-			buttonPanel.add(forwardButton);
-			URLField = new JTextField(35);
-			buttonPanel.add(URLField);
-			JButton returnButton = new JButton("Return");
+		 
+			displayEditorPane = new JEditorPane();
+		     displayEditorPane.setContentType("text/html");
+		     displayEditorPane.setEditable(false);
+		     displayEditorPane.addHyperlinkListener(this);
+		    
 	}
 
 	private void setupLayout()
 	{
+		 getContentPane().setLayout(new BorderLayout());
+	     getContentPane().add(buttonPanel, BorderLayout.NORTH);
+	     getContentPane().add(new JScrollPane(displayEditorPane), BorderLayout.CENTER);
 		appLayout.putConstraint(SpringLayout.NORTH, descriptionArea, 9, SpringLayout.SOUTH, titleLabel);
 		appLayout.putConstraint(SpringLayout.NORTH, titleLabel, 32, SpringLayout.NORTH, this);
 		appLayout.putConstraint(SpringLayout.WEST, titleLabel, 172, SpringLayout.WEST, this);
@@ -108,7 +253,7 @@ public class HistoryPanel extends JPanel implements HyperlinkListener
 		appLayout.putConstraint(SpringLayout.WEST, inputField, 66, SpringLayout.WEST, this);
 		appLayout.putConstraint(SpringLayout.NORTH, close, 12, SpringLayout.SOUTH, viewLocalHistory);
 	}
-	//*Abstraction
+	
 	private void setupListeners()
 	{
 		viewLocalHistory.addActionListener(new ActionListener()
@@ -181,6 +326,13 @@ public class HistoryPanel extends JPanel implements HyperlinkListener
 					{
 						actionGo();
 					}
+				}
+			});
+			returnButton.addActionListener(new ActionListener()
+			{
+				public void actionPerformed(ActionEvent event)
+				{
+				actionGo();
 				}
 			});
 	}
